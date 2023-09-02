b612cbeed90e198539d8455817b9590307798fd9
Iamneo <techadmin@iamneo.ai>
2023-09-02 18:38:49 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,29 +1,35 @@
-
+// JavaScript logic for Tic Tac Toe game
 const cells = document.querySelectorAll('.cell');
 const resultText = document.getElementById('result');
 const resetButton = document.getElementById('reset-btn');
 
 let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameOver = false;
-// Function to handle cell click
-function handleCellClick(index) {
-    const cell = cells[index];
-    if (cell.value === '' && !gameOver) {
-        cell.value = currentPlayer;
 
-        if (checkWin(currentPlayer)) {
-            resultText.textContent = `Player ${currentPlayer} wins!`;
-            gameOver = true;
-        } else if ([...cells].every(cell => cell.value !== '')) {
-            resultText.textContent = "It's a draw!";
-            gameOver = true;
-        } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            resultText.textContent = `Player ${currentPlayer}'s Turn`;
-        }
+function handleMove(index) {
+    if (gameBoard[index] === '' && gameActive) {
+      gameBoard[index] = currentPlayer;
+      const clickedButton = document.getElementsByClassName('btn')[index];
+      
+      // Set the button's text to the current player's symbol
+      clickedButton.innerText = currentPlayer;
+      
+      // Check for a win or draw
+      if (checkWin()) {
+        gameActive = false;
+        displayResult(`${currentPlayer} wins!`);
+      } else if (checkDraw()) {
+        gameActive = false;
+        displayResult("It's a draw!");
+      } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        updateTurnDisplay();
+      }
     }
-}
-
+  }
+  
+// Function to check for a win
 function checkWin(player) {
     const winPatterns = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -32,11 +38,13 @@ function checkWin(player) {
     ];
 
     return winPatterns.some(pattern => {
-        return pattern.every(index => cells[index].value === player);
+        return pattern.every(index => gameBoard[index] === player);
     });
 }
 
+// Function to reset the game
 function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
     cells.forEach(cell => {
         cell.value = '';
         cell.disabled = false;
