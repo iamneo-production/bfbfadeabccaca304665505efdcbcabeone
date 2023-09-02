ac25e7e7190d7c774ce0ce0a061261801784417e
Iamneo <techadmin@iamneo.ai>
2023-09-02 18:33:49 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,57 +1,10 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
+
+const cells = document.querySelectorAll('.cell');
+const resultText = document.getElementById('result');
+const resetButton = document.getElementById('reset-btn');
+
 let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let cellsElements = document.querySelectorAll('.cell'); 
-let gameOver= false;
-
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    if (cells[index] === '' && !checkWin()) {
-        cells[index] = currentPlayer;
-        element.textContent = currentPlayer;
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        // Display the current player's turn
-        result.textContent = `Current Player: ${currentPlayer}`;
-    }
-};
-
-// Function to check for a win
-const checkWin = () => {
-    for (const condition of conditions) {
-        const [a, b, c] = condition;
-        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-            result.textContent = `${currentPlayer} wins!`;
-            cellsElements.forEach(cell => cell.removeEventListener('click', handleClick)); // Disable cell clicks
-            return true; // Return true to indicate a win
-        }
-    }
-    return false; // No winner yet
-};
-
-// Function to reset the game
-const resetGame = () => {
-    cells = ['', '', '', '', '', '', '', '', ''];
-    cellsElements.forEach(cell => {
-        cell.textContent = '';
-        cell.addEventListener('click', handleClick); // Re-enable cell clicks
-    });
-    currentPlayer = 'X';
-    result.textContent = 'Current Player: X';
-};
-
-// Function to handle cell clicks
+let gameOver = false;
 // Function to handle cell click
 function handleCellClick(index) {
     const cell = cells[index];
@@ -71,7 +24,34 @@ function handleCellClick(index) {
     }
 }
 
-// Add click event listeners to cells
-cellsElements.forEach(cell => cell.addEventListener('click', handleClick));
+function checkWin(player) {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
 
-document.querySelector('#reset').addEventListener('click', resetGame);
+    return winPatterns.some(pattern => {
+        return pattern.every(index => cells[index].value === player);
+    });
+}
+
+function resetGame() {
+    cells.forEach(cell => {
+        cell.value = '';
+        cell.disabled = false;
+    });
+    currentPlayer = 'X';
+    resultText.textContent = `Player ${currentPlayer}'s Turn`;
+    gameOver = false;
+}
+
+// Add event listeners to cells
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => {
+        handleCellClick(index);
+    });
+});
+
+// Add event listener to reset button
+resetButton.addEventListener('click', resetGame);
